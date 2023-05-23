c0069d5f8fc21611d387ba7e29c3b77b5fcc4e55
Raymond Capozzi <rcapozzi@gmail.com>
2023-05-23 09:06:38 -0400

    WTF

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 # https://realpython.com/python-dash/
 # https://www.pythonanywhere.com/user/rcapozzi/
+import warnings
+warnings.simplefilter(action='ignore', category=UserWarning)
 
 import plotly.express as px
 from plotly.subplots import make_subplots
@@ -10,12 +12,13 @@ import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 import datetime
+import math
 import pytz
 from dash import Dash, html, dcc, Output, Input, dash_table
 import dash_bootstrap_components as dbc
 import logging
-
 from utils import OptionQuotes
+import random
 
 def console_logger():
     # create console handler and set level to debug
@@ -52,25 +55,51 @@ def transformToLadder(df):
     dfcp.pop('putCall_p')
     return dfcp
 
-def dash_layout():
+import glob
+import re
+def get_files():
+    file_dict = {}
+    #for filename in os.listdir('../tda-tbd/data/*.parquet'):
+    for filename in glob.glob('../tda-tbd/data/*.parquet'):
+        filename = filename.replace('\\', '/')
+        match = re.search(r"([^/]+)\.parquet$", filename)
+        if match:
+            key = match.group(1)
+            file_dict[key] = filename
+    keys = sorted(file_dict.keys(), reverse=True)[:5]
+    file_dict = {key: value for key, value in file_dict.items() if key in keys}
+    return file_dict
 
+def dash_layout():
     symbols = ['SPX.X', 'SPY']
     symbols = ['SPX.X']
 
     app.OptionQuotes = {}
     try:
-        for s in symbols:
-            yyyymmdd = datetime.datetime.today().strftime('%Y-%m-%d')
-            filename = f'data/{s}.{yyyymmdd}.parquet'
-            app.OptionQuotes[s] = OptionQuotes(symbol=s,filename=filename)
+        # for s in symbols:
+        #     yyyymmdd = datetime.datetime.today().strftime('%Y-%m-%d')
+        #     filename = f'data/{s}.{yyyymmdd}.parquet'
+        #     if os.path.isfile(filename):
+        #         app.OptionQuotes[s] = OptionQuotes(symbol=s,filename=filename)
+
+        files = get_files()
+        for k, v in files.items():
+            # app.logger.info(f'files k={k} v={v}')
+            app.OptionQuotes[k] = OptionQuotes(symbol=k,filename=v)
+            app.OptionQuotes[k].data.file_label = k
+        symbols += sorted(files.keys(), reverse=True)
+        symbols = sorted(app.OptionQuotes.keys(), reverse=True)
+
     except Exception as e:
         return html.Div([
             html.Hr(),
-            html.H1(children="You filthy Degen. Its too Soon. Check back during market hours."),
+            html.H1("You filthy Degen. Check back during market hours."),
+            #html.Span(f'No file for {filename}'),
+            html.Span(f'{e}', style={'padding': '5px', 'fontsize:': '10px'}),
         ])
 
-    xfields = [ 'processDateTime', 'underlyingPrice', 'strikePrice']
-    yfields = [ 'volume', 'markVol', 'gexVol', 'mark', 'totalVolume', 'delta' ]
+    xfields = [ 'processDateTime']#, 'underlyingPrice', 'strikePrice']
+    yfields = [ 'volume'] #, 'markVol', 'gexVol', 'mark', 'totalVolume', 'delta' ]
 
     return html.Div(
         children=[
@@ -129,7 +158,7 @@ def dash_layout():
             ),
             html.Div(id='strikes-selector-div', className="card"),
             html.Div(dcc.Graph(id="pc-summary", config={"displayModeBar": False}), className="card",),
-            html.Div(dcc.Graph(id="pc-net", config={"displayModeBar": False}), className="card",),
+            html.Div(dcc.Graph(id="pc-net-volume", config={"displayModeBar": False}), className="card",),
             #html.Div(dcc.Graph(id="pc-put", config={"displayModeBar": False}), className="card",),
             # html.Div(children = dcc.Graph(id="scatter3d-call", config={"displayModeBar": False}), className="card", ),
             # html.Div(children = dcc.Graph(id="scatter3d-put", config={"displayModeBar": False}), className="card", ),
@@ -172,11 +201,16 @@ def update_strikesselector(symbol):
     """TODO suppress_callback_exceptions=True"""
     #logger.info(f"enter {symbol}")
     df = app.OptionQuotes[symbol].data
+    df = df.loc[(df.totalVolume > 10)]
     min, max = df.strikePrice.min(), df.strikePrice.max()
     priceMin, priceMax = int(df.underlyingPrice.min()), int(df.underlyingPrice.max())
     priceRange = priceMax - priceMin
     if priceRange > 10 or priceMax > 1000:
         step_size = 25
+        min = math.floor(min / step_size) * step_size
+        max = math.floor(max / step_size) * step_size
+        priceMin = math.floor(priceMin / step_size) * step_size
+        priceMax = math.ceil(priceMax / step_size) * step_size
     else:
         step_size = 1
 
@@ -188,38 +222,13 @@ def update_strikesselector(symbol):
                 tooltip={"placement": "bottom", "always_visible": True}, id='strikes-rangeslider')
     ]
 
-# def brainfuck(df, pc_filter, strikes, xaxis, yaxis, zaxis):
-#     return {}
-#     data = df[(df.putCall == pc_filter) & (df.strikePrice >= strikes[0]) & (df.strikePrice <= strikes[1])]
-#     fig = px.scatter_3d(data, x=xaxis, y=yaxis, z=zaxis, color='symbol', size='volume')
-
-#     data = df[(df.putCall == 'CALL') & (df.strikePrice >= strikes[0]) & (df.strikePrice <= strikes[1])]
-#     trace1 = go.Scatter3d(x=data[xaxis], y=data[yaxis], z=data[zaxis])
-
-#     data = df[(df.putCall == 'PUT') & (df.strikePrice >= strikes[0]) & (df.strikePrice <= strikes[1])]
-#     trace2 = go.Scatter3d(x=data[xaxis], y=data[yaxis], z=data[zaxis])
-
-#     fig = make_subplots(rows=1, cols=2, specs=[[{'type': 'scatter3d'}, {'type': 'scatter3d'}]])
-#     fig.add_trace(trace1, row=1, col=1)
-#     fig.add_trace(trace2, row=1, col=2)
-#     fig.update_layout(margin=dict(l=0, r=0, b=0, t=0), height = 600, width = 800)
-
-#     d0 = data.processDateTime.min()
-#     d1 = data.processDateTime.max()
-#     fig.update_layout(
-#         xaxis = dict(
-#             autorange=True,
-#             range=[d0, d1],
-#             rangeslider=dict(autorange=True, range=[d0, d1]),
-#             type="date"
-#         ))
-#     return fig
-
 def chart_net_volume(df, yaxis):
-    now_dt = datetime.datetime.now(pytz.timezone('US/Eastern'))
-    if now_dt.time().hour < 16:
-        cutoff_dt = now_dt - datetime.timedelta(minutes=120)
-        df = df.loc[df.processDateTime > cutoff_dt]
+    # now_dt = datetime.datetime.now(pytz.timezone('US/Eastern'))
+    # app.logger.info(f'chart_net_volume enter {now_dt}')
+    # if now_dt.time().hour < 16:
+    #     cutoff_dt = now_dt - datetime.timedelta(minutes=120)
+    #     df = df.loc[df.processDateTime > cutoff_dt]
+    yaxis = 'totalVolume'
     s = df.groupby(['putCall', 'processDateTime'])[yaxis].sum()
     fig = make_subplots(specs=[[{"secondary_y": False}]])
 
@@ -233,11 +242,13 @@ def chart_net_volume(df, yaxis):
 
     net = net.rolling(10).mean()
     fig.add_trace(go.Scattergl(x=calls.index, y=net.values, name='SMA10', marker_color='yellow') )
-
     fig.update_layout(title_text=f'SPX Put/Call {yaxis}', template='plotly_dark', height=600)
-
     return fig
 
+# Idealy return 
+# {'x': '2023-05-19 09:56', 'y': [{'SPXW_051923P4250': -9 }, 'SPXW_051923C4250': 75 }] }
+# { 'name': 'SPXW_051923P4250', 'x': datetime.datetime(2023, 5, 19, 9, 46), 'y': -9 },
+# { 'name': 'SPXW_051923C4250', 'x': datetime.datetime(2023, 5, 19, 9, 46), 'y': 75 }
 def chart_pc_summary(df, strikes, yaxis):
     """
     import plotly.io as pio
@@ -253,7 +264,7 @@ def chart_pc_summary(df, strikes, yaxis):
     symbols = data.symbol.sort_values().unique()
 
     for s in symbols:
-        sign = 1 if data[(data.symbol == s)].iloc[1].putCall == 'CALL' else -1
+        sign = 1 if data[(data.symbol == s)].iloc[0].putCall == 'CALL' else -1
         x=data[(data.symbol == s)][xaxis].dt.tz_localize(tz=None)
         y=data[(data.symbol == s)][yaxis]
         maxVal =y.max()
@@ -272,12 +283,14 @@ def chart_pc_summary(df, strikes, yaxis):
     fig['layout']['yaxis2']['showgrid'] = False
     fig.update_yaxes(title_text="<b>underlyingPrice</b>", secondary_y=True)
     fig.update_xaxes(tickformat="%H:%M")
-    fig.update_layout(title_text=f'SPX Call/Put Pez Dispenser {yaxis}', template='plotly_dark', height=600)
+    fig.update_layout(title_text=f'SPX Call/Put Pez Dispenser {df._pretty_name} {yaxis}', template='plotly_dark', height=600)
+    #x = datetime.datetime.now(pytz.timezone('US/Eastern'))
+    #app.logger.info(f'chart_pc_summary returns x={x}')
     return fig
 
 @app.callback(
     Output("pc-summary", "figure"),
-    Output("pc-net", "figure"),
+    Output("pc-net-volume", "figure"),
     Input("symbol", "value"),
     Input("strikes-rangeslider", "value"),
     Input("x-axis", "value"),
@@ -285,16 +298,14 @@ def chart_pc_summary(df, strikes, yaxis):
     Input('graph-update','n_intervals'), prevent_initial_call=True
 )
 def update_charts(symbol, strikes, xaxis, yaxis, n):
-    #logger.info(f"enter {symbol}")
     df = app.OptionQuotes[symbol].reload()
-    fig_summary = chart_pc_summary(df, strikes, yaxis)
-    fig_net_volume = chart_net_volume(df, yaxis)
-
-    # fig_3dcalls = brainfuck(df, 'CALL', strikes, xaxis, yaxis, zaxis)
-    # fig_3dputs = brainfuck(df, 'PUT', strikes, xaxis, yaxis, zaxis)
+    if not hasattr(df, '_fig_summary'):
+        #FIX: Issues UserWarning
+        df._fig_summary = chart_pc_summary(df, strikes, yaxis)
+    if not hasattr(df, '_fig_net_volume'):
+        df._fig_net_volume = chart_net_volume(df, yaxis)
     #fig_call.update_layout(height=600, width = 800)
-
-    return [fig_summary, fig_net_volume]
+    return [df._fig_summary, df._fig_net_volume]
 
 def calc_interval_to_update():
     # now_dt = datetime.datetime.now(pytz.timezone('US/Eastern'))
@@ -314,9 +325,10 @@ def calc_interval_to_update():
 def update_metrics(*args):
     symbol = args[0]
     style_metrics = {'padding': '5px', 'fontsize:': '10px'}
-    df = app.OptionQuotes[symbol].data
+    oq = app.OptionQuotes[symbol]
+    df = oq.data
     maxProcessDateTime = df.processDateTime.max()
-    interval = 120
+    interval = 10
 
     return [
         html.Span(dcc.Interval(id='graph-update', interval=interval*1000)),
@@ -325,6 +337,10 @@ def update_metrics(*args):
         html.Span(f"Range: {df.underlyingPrice.min()}/{df.underlyingPrice.max()}", style=style_metrics),
         html.Span(f"Strikes: {df.strikePrice.min()}/{df.strikePrice.max()}", style=style_metrics),
         html.Span(f"Refresh: {interval}s", style=style_metrics),
+        # html.Div([
+        #     html.Span(f"filename:{app.OptionQuotes.filename}", style=style_metrics),
+        #     html.Span(f"size:{len(df)}", style=style_metrics),
+        #     ]),
         ]
 
 if __name__ == '__main__':
